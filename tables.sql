7cf5fc5cb64900d8a1225d143bed620528d6515a
LuyoloLuja <luyolom19@gmail.com>
2020-11-12 18:28:47 +0200

    created my tables

--- tables.sql
+++ tables.sql
@@ -1,5 +1,25 @@
-CREATE TABLE waiter_info (
+CREATE TABLE waiter_names (
     id SERIAL NOT NULL PRIMARY KEY,
-    waiter_name TEXT NOT NULL,
-    work_days TEXT NOT NULL
-)
+    waiter_name TEXT NOT NULL
+);
+
+CREATE TABLE waiter_days (
+    id SERIAL NOT NULL PRIMARY KEY,
+    day_working TEXT NOT NULL
+);
+INSERT INTO waiter_days (day_working) VALUES ("Monday");
+INSERT INTO waiter_days (day_working) VALUES ("Tuesday");
+INSERT INTO waiter_days (day_working) VALUES ("Wednesday");
+INSERT INTO waiter_days (day_working) VALUES ("Thursday");
+INSERT INTO waiter_days (day_working) VALUES ("Friday");
+INSERT INTO waiter_days (day_working) VALUES ("Saturday");
+INSERT INTO waiter_days (day_working) VALUES ("Sunday");
+
+CREATE TABLE working_days (
+    id SERIAL NOT NULL PRIMARY KEY,
+    shift TEXT NOT NULL,
+    waiter_name INT NOT NULL,
+    FOREIGN KEY (waiter_name) REFERENCES waiter_names(id),
+    days_working INT NOT NULL,
+    FOREIGN KEY (days_working) REFERENCES waiter_days(id)
+);
--- waiter-app.js
+++ waiter-app.js
@@ -1,3 +1,17 @@
 module.exports = function WaiterApp(pool){
 
+    async function waiterName(name){
+        
+        if(name){
+            let nameEntered = await pool.query('SELECT waiter_name FROM waiter_names WHERE waiter_name = $1', [name]);
+
+            if(nameEntered.rowCount === 0){
+                await pool.query('INSERT INTO waiter_names (waiter_name) VALUES ($1)', [name])
+            }
+        }
+    }
+
+    return {
+        waiterName
+    }
 }
