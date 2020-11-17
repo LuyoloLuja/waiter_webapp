d85d83275e760572a6848610e346d0a65a66ae33
LuyoloLuja <luyolom19@gmail.com>
2020-11-17 09:44:03 +0200

    fixed id of undefined error

--- index.js
+++ index.js
@@ -27,33 +27,23 @@ app.get('/', function (req, res) {
 app.post('/waiters/:username', async function (req, res) {
   let name = req.body.name;
   let days = req.body.day;
-//  console.log(days);
-//   const id =  await waiterAppInstance.getNameId(name)
-//   console.log(i);
+
   await waiterAppInstance.addWaiterInfo(name, days);
 
-  // let userName = await waiterAppInstance.getNameId(name);
-  // let userDays = await waiterAppInstance.getDaysId(days);
-
-
-  // let userDetails = await waiterAppInstance.storedDetails()
-
   res.render('home', {
-    // waiter_name: userName,
-    // days_working: userDays,
-    // userDetails
+    
   })
 })
 
-app.get('/waiters/:username', async function (req, res) {
-  let userDetails = await waiterAppInstance.storedDetails();
+// app.get('/waiters/:username', async function (req, res) {
+//   let userDetails = await waiterAppInstance.storedDetails();
 
-  res.render(`/waiters'/${username}`, {
-    waiter_name: userDetails,
-    days_working: userDetails
-  })
+//   res.render(`/waiters'/${username}`, {
+//     waiter_name: userDetails,
+//     days_working: userDetails
+// })
 
-});
+// });
 
 let PORT = process.env.PORT || 3032;
 
--- waiter-app.js
+++ waiter-app.js
@@ -3,25 +3,14 @@ module.exports = function WaiterApp(pool) {
     async function addWaiterInfo(name, days) {
 
         if (name && days) {
-            // let nameEntered = await pool.query('SELECT waiter_name FROM working_days WHERE waiter_name = $1', [name]);
-            // let daysWorking = await pool.query('SELECT days_working FROM working_days WHERE days_working = $1', [days]);
             let namesId = await getNameId(name);
 
             for (const eachDay of days) {
-                console.log(eachDay);
+
                 let daysId = await getDaysId(eachDay);
-                console.log(daysId);
+
                 await pool.query('INSERT INTO working_days (waiter_name, days_working) VALUES ($1, $2)', [namesId, daysId]);
-
-                // return eachDay;
             }
-
-            // let daysId = await getDaysId(days);
-
-            // if (namesId.rowCount === 0 && daysId.rowCount === 0) {
-            // } else {
-            // await pool.query('UPDATE working_days waiter_name SET days_working = days_working + 1 WHERE waiter_name = $1', [name, daysWorking]);
-            // }
         }
     }
 
@@ -32,6 +21,7 @@ module.exports = function WaiterApp(pool) {
         if (nameEntered.rowCount === 0) {
             await pool.query('INSERT INTO waiter_names (waiter_name) VALUES ($1)', [name]);
         }
+
         var namesId = await pool.query('SELECT id FROM waiter_names WHERE waiter_name = $1', [name]);
         return namesId.rows[0].id;
     }
@@ -39,8 +29,8 @@ module.exports = function WaiterApp(pool) {
     async function getDaysId(days) {
         try {
             let daysId = await pool.query('SELECT id FROM days_of_work WHERE day_working = $1', [days]);
-            // console.log(daysId.rows[0].id);
             return daysId.rows[0].id;
+            
         } catch (error) {
             console.log(error);
         }
